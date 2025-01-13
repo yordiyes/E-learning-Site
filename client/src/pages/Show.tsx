c6fcf4100596b5798581ef35abf66f8562bc8077
Yordanos Yeshitla <yordiman614@gmail.com>
2025-01-13 22:14:45 +0300

    change

--- client/src/pages/Show.tsx
+++ client/src/pages/Show.tsx
@@ -9,7 +9,7 @@ export default function Show() {
           <h1 className="text-5xl pt-5 px-5 font-bold lg:text-6xl sm:text-4xl">
             Develop your skills <br /> in a new <br /> and unique way
           </h1>
-          <p className="mt-4 py-5 px-5">
+          <p className="mt-4 py-5 px-5 text-lg ">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
             praesentium error tenetur quasi quas sed optio, eligendi provident
             perspiciatis magnam dolore repellat, numquam dolores vitae,
@@ -17,6 +17,7 @@ export default function Show() {
             Accusantium, delectus assumenda.
           </p>
           <button className="border-2 border-none text-Tan px-4 mx-12 py-2 text-2xl rounded hover:bg-blue-500 bg-gradient-to-r from-purple-400 to-pink-300 hover:text-white transition">Enroll Now</button>
+          <a href="#" className="underline">What's E-learning?</a>
         </div>
 
         <img src={student} className="stud-img" alt="" />
