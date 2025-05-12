2175400cb1b5b044e31dec14366b350ab80f16f4
Yordanos Yeshitla <yordiman614@gmail.com>
2025-05-12 11:05:49 +0300

    Refactor user model for consistency and update MongoDB connection to use environment variable

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,28 +1,31 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+const MUNGODB_URI = process.env.MONGODB_URI;
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { 
-      type: String, 
-      required: function() { 
-        // Password is required only if the user doesn't log in through Google
-        return !this.googleId; 
-      }
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: {
+    type: String,
+    required: function () {
+      // Password is required only if the user doesn't log in through Google
+      return !this.googleId;
     },
-    role: { type: String, default: "student" },
-    googleId: { type: String }, // Google ID for OAuth users
-  });
-  
+  },
+  role: { type: String, default: "student" },
+  googleId: { type: String }, // Google ID for OAuth users
+});
 
-const Users = mongoose.model('Users', userSchema);
+const Users = mongoose.model("Users", userSchema);
 
-mongoose.connect("mongodb://127.0.0.1:27017/E-learning")
-.then(()=>{
+mongoose
+  .connect(MUNGODB_URI)
+  .then(() => {
     console.log("Database connected");
-})
-.catch((error)=>{
-    console.log("Error connecting to databacse" + error);   
-})
+  })
+  .catch((error) => {
+    console.log("Error connecting to databacse" + error);
+  });
 
 export default Users;
